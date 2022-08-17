faaea1d5b46cf9d5fb97cd7e97b8cb04c4f940d9
deliso <s.moraleslopez.bcn@gmail.com>
2022-08-17 11:49:42 +0200

    style(web app): add theme for mui and change color palette

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AdapterLuxon } from '@mui/x-date-pickers/AdapterLuxon';
 
 import './App.css';
+import { ThemeProvider } from '@emotion/react';
 import { LocalizationProvider } from '@mui/x-date-pickers';
+import { createTheme } from '@mui/material';
 import LandingPage from './pages/LandingPage';
 import PrivateRoutes from './utils/PrivateRoutes';
 import ReduxSample from './ReduxSample';
@@ -11,22 +13,35 @@ import Login from './pages/Login';
 import TripForm from './pages/Dashboard/TripForm';
 import Discover from './pages/Discover/Discover';
 
+const primaryColor = '#1CB985';
+
+const appTheme = createTheme({
+	palette: {
+		primary: {
+			main: primaryColor,
+			contrastText: '#fff',
+		},
+	},
+});
+
 function App() {
 	return (
 		<div className="App">
 			<LocalizationProvider dateAdapter={AdapterLuxon}>
-				<Router>
-					<Routes>
-						<Route path="/" element={<LandingPage />} />
-						<Route path="/login" element={<Login />} />
-						<Route path="/discover/*" element={<Discover />} />
-						<Route path="/redux" element={<ReduxSample />} />
-						<Route path="/newtrip" element={<TripForm />} />
-						<Route element={<PrivateRoutes />}>
-							<Route path="/private" element={<Private />} />
-						</Route>
-					</Routes>
-				</Router>
+				<ThemeProvider theme={appTheme}>
+					<Router>
+						<Routes>
+							<Route path="/" element={<LandingPage />} />
+							<Route path="/login" element={<Login />} />
+							<Route path="/discover/*" element={<Discover />} />
+							<Route path="/redux" element={<ReduxSample />} />
+							<Route path="/newtrip" element={<TripForm />} />
+							<Route element={<PrivateRoutes />}>
+								<Route path="/private" element={<Private />} />
+							</Route>
+						</Routes>
+					</Router>
+				</ThemeProvider>
 			</LocalizationProvider>
 		</div>
 	);
